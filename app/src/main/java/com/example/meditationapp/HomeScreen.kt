3a24abe73720e280b6b3bf09fe7ec25634dac69e
Mohammed <mo7amxd.7@gmail.com>
2024-07-25 23:06:08 +0100

    Top App Bar And Chip Section

--- app/src/main/java/com/example/meditationapp/HomeScreen.kt
+++ app/src/main/java/com/example/meditationapp/HomeScreen.kt
@@ -3,7 +3,9 @@ package com.example.meditationapp
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
@@ -22,7 +24,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,6 +34,7 @@ import androidx.compose.ui.unit.dp
 import com.example.meditationapp.ui.theme.ButtonBlue
 import com.example.meditationapp.ui.theme.DarkerButtonBlue
 import com.example.meditationapp.ui.theme.DeepBlue
+import com.example.meditationapp.ui.theme.LightRed
 import com.example.meditationapp.ui.theme.TextWhite
 
 @Composable
@@ -42,6 +47,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         Column {
             TopAppBar()
             ChipSection(chipTexts = listOf("All", "Favorites"))
+            CurrentMeditation()
 
 
         }
@@ -81,13 +87,12 @@ fun TopAppBar(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ChipSection(chipTexts: List<String>,modifier: Modifier = Modifier) {
+fun ChipSection(chipTexts: List<String>, modifier: Modifier = Modifier) {
 
     LazyRow {
-        items(chipTexts.size) {index ->
+        items(chipTexts.size) { index ->
             Chip(chipTxt = chipTexts[index])
         }
-
 
 
     }
@@ -96,7 +101,42 @@ fun ChipSection(chipTexts: List<String>,modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Chip(chipTxt: String,modifier: Modifier = Modifier) {
+fun CurrentMeditation(modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(LightRed)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(1f).padding(15.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = "Daily Thought", style = MaterialTheme.typography.titleMedium, color = TextWhite
+            )
+            Text(
+                text = "Meditation 3-5 min", style = MaterialTheme.typography.titleMedium, color = TextWhite
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(10.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_play),
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+
+            )
+        }
+
+
+    }
+}
+
+@Composable
+fun Chip(chipTxt: String, modifier: Modifier = Modifier) {
     var selected by remember {
         mutableStateOf(false)
 
